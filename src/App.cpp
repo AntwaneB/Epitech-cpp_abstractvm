1bc8f7e6d19784b88f69442218b01edace95d893
François Kiene <francois.kiene@epitech.eu>
2015-02-09 14:02:09 +0100

    Update App

--- src/App.cpp
+++ src/App.cpp
@@ -8,6 +8,11 @@
  ** Updated on February 6, 2015, 10:53 AM Antoine Buchser
  */
 
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
 #include "App.hpp"
 
 App::App(int ac, char** av)
@@ -22,7 +27,36 @@ App::~App()
 
 int	App::run()
 {
-	
+std::ifstream					ofs;
+std::vector<std::string> 		vector;
+std::string 					save;
+int								i;
 
-	return (0);
+i = 1;
+if (_ac < 2)
+{
+	while (save != ";;")
+	{
+		std::cin >> save;
+		vector.push_back(save);
+	}
+	if (save == ";;")
+		Parser(vector);
+}
+else
+{
+	while (i < _ac)
+		{
+		ofs.open(_av[i]);
+		if (ofs.is_open())
+			{
+			for(std::string line; getline(ofs, line);)
+				vector.push_back(line);
+			Parser(vector);
+			ofs.close();
+			}
+		i++;
+		}
+}
+return (0);
 }
